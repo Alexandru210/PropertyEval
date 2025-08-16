8332fa8d3543837c7676d36c2a70ce9ec1ed10cf
Alexandru210 <101139272+Alexandru210@users.noreply.github.com>
2025-08-16 16:23:21 +0300

    removed UserRepository, added UserService and interface

---
+++ PropertyEval.Application/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using PropertyEval.Domain.Entities;
+
+namespace PropertyEval.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> CreateUserAsync(string FirstName, string LastName, string Email, string PasswordHash, CancellationToken cancellationToken);
+    }
+}
---
+++ PropertyEval.Infrastructure/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using PropertyEval.Application.Interfaces;
+using PropertyEval.Domain.Entities;
+using PropertyEval.Infrastructure.Data;
+
+namespace PropertyEval.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _db;
+
+        public UserService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<User> CreateUserAsync(string FirstName, string LastName, string Email, string PasswordHash, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
