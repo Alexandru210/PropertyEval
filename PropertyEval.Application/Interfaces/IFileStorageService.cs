f535e7bd4e6af32eb32ae291fec5add91964095e
Alexandru210 <101139272+Alexandru210@users.noreply.github.com>
2025-08-17 02:11:32 +0300

    add IFileStorageService

---
+++ PropertyEval.Application/Interfaces/IFileStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertyEval.Application.Interfaces
+{
+    public interface IFileStorageService
+    {
+        Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType, CancellationToken cancellationToken);
+        Task DeleteFileAsync(string fileUrl, CancellationToken cancellationToken);
+    }
+}
