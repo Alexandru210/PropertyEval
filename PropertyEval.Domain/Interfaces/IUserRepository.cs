19bbfdd0b2f8a947f3b691aa4092bbeb9c5d31f1
Alexandru210 <101139272+Alexandru210@users.noreply.github.com>
2025-08-16 03:56:29 +0300

    initial migration

---
+++ PropertyEval.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using PropertyEval.Domain.Entities;
+
+namespace PropertyEval.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<User> AddAsync(User user, CancellationToken cancellationToken = default);
+        Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
