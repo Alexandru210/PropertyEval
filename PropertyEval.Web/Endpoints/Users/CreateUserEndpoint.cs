fc65fdcd9706fd9b5861148ca88bfd4446fa2518
Alexandru210 <101139272+Alexandru210@users.noreply.github.com>
2026-02-22 23:36:07 +0200

    added editorconfig

--- PropertyEval.Web/Endpoints/Users/CreateUserEndpoint.cs
+++ PropertyEval.Web/Endpoints/Users/CreateUserEndpoint.cs
@@ -2,47 +2,46 @@
 using PropertyEval.Application.DTOs;
 using PropertyEval.Infrastructure.Services;
 
-namespace PropertyEval.Web.Endpoints.Users
+namespace PropertyEval.Web.Endpoints.Users;
+
+public class CreateUserEndpoint : Endpoint<CreateUserRequest, UserResponse>
 {
-    public class CreateUserEndpoint : Endpoint<CreateUserRequest, UserResponse>
+    private readonly UserService _userService;
+
+    public CreateUserEndpoint(UserService userService)
     {
-        private readonly UserService _userService;
-
-        public CreateUserEndpoint(UserService userService)
+        _userService = userService;
+    }
+    public override void Configure()
+    {
+        Post("/users");
+        AllowAnonymous();
+        Description(x => x
+            .WithName("CreateUser")
+            .Produces<UserResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status409Conflict)
+            );
+    }
+    public override async Task HandleAsync(CreateUserRequest request, CancellationToken ct)
+    {
+        try
         {
-            _userService = userService;
+            var result = await _userService.CreateUserAsync(request, ct);
+
+            var response = new UserResponse(
+                result.Id,
+                result.FirstName,
+                result.LastName,
+                result.Email
+            );
+
+            await Send.CreatedAtAsync<CreateUserEndpoint>(responseBody: response, cancellation: ct);
         }
-        public override void Configure()
+        catch (InvalidOperationException ex)
         {
-            Post("/users");
-            AllowAnonymous();
-            Description(x => x
-                .WithName("CreateUser")
-                .Produces<UserResponse>(StatusCodes.Status201Created)
-                .ProducesProblem(StatusCodes.Status400BadRequest)
-                .ProducesProblem(StatusCodes.Status409Conflict)
-                );
-        }
-        public override async Task HandleAsync(CreateUserRequest request, CancellationToken ct)
-        {
-            try
-            {
-                var result = await _userService.CreateUserAsync(request, ct);
-
-                var response = new UserResponse(
-                    result.Id,
-                    result.FirstName,
-                    result.LastName,
-                    result.Email
-                );
-
-                await Send.CreatedAtAsync<CreateUserEndpoint>(responseBody: response, cancellation: ct);
-            }
-            catch (InvalidOperationException ex)
-            {
-                AddError(ex.Message);
-                await Send.ErrorsAsync(StatusCodes.Status409Conflict, ct);
-            }
+            AddError(ex.Message);
+            await Send.ErrorsAsync(StatusCodes.Status409Conflict, ct);
         }
     }
 }
