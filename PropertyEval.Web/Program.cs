688fdb36f4448b753b3c4f87f6425dfe2112ca70
Alexandru210 <101139272+Alexandru210@users.noreply.github.com>
2026-05-24 20:32:38 +0300

    added cors

--- PropertyEval.Web/Program.cs
+++ PropertyEval.Web/Program.cs
@@ -6,6 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
 // Register FastEndpoints and Swagger
 builder.Services.AddFastEndpoints();
 builder.Services.SwaggerDocument();
@@ -13,6 +25,7 @@ builder.Services.SwaggerDocument();
 var app = builder.Build();
 
 // Middleware pipeline
+app.UseCors("DefaultPolicy");
 app.UseFastEndpoints();
 app.UseSwaggerGen();
 
